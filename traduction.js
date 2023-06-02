08dfb06fd27a53f81031a7b944c640ba5ccc4816
izMind <128811177+izMind@users.noreply.github.com>
2023-06-02 13:47:22 -0400

    Create traduction.js

---
+++ traduction.js
@@ -0,0 +1,28 @@
+export default {
+fr:{
+  headText: "INSCRIPTION AUX COURS",
+  noStudent: "Aucun étudiant sélectionné",
+  selectStudent: "SÉLECTIONNER UN ÉTUDIANT",
+  confirmStudent: "Confirmez votre sélection",
+  studentSelected: "Élève sélectionné",
+  semester: "Session",
+  registerStudent:" Enregistrer élève au cours:",
+  register:"ENREGISTRER",
+  display:"AFFICHER",
+  submit:"Soumis",
+  studentClass: "prend les cours: "
+},
+en:{
+  headText: "CLASS REGISTRATION",
+  noStudent: "No student selected",
+  selectStudent: "SELECT A STUDENT",
+  confirmStudent: "Confirm you selection",
+  studentSelected: "Selection confirmed",
+  semester: "Semester",
+  registerStudent:" Register student to class:",
+  register:"REGISTER",
+  display:"DISPLAY",
+  submit:"Submit",
+  studentClass: "take the classes: "
+  }
+}
